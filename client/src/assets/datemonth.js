5186dda336b31e27f295a42637f09ac617c27840
sayak <Sayakraha12@gmail.com>
2024-01-16 17:05:16 +0530

    jh

--- client/src/assets/datemonth.js
+++ client/src/assets/datemonth.js
@@ -2,9 +2,9 @@ const DateinMonth = () => {
     const currentYear = new Date().getFullYear();
 const isLeapYear = (currentYear % 4 === 0 && currentYear % 100 !== 0) || (currentYear % 400 === 0);
 
-const febdays = isLeapYear ? 29 : 28;
+const febday = isLeapYear ? 29 : 28;
     return [31,
-        febdays,
+        febday,
          31,
         30,
         31,
